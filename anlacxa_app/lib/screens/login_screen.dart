35e6553656d662aa93a09ebbf8e59a8cdfca7c9f
Khang Pham <nguyentuanngoc@NGUYENs-MacBook-Air.local>
2024-06-19 18:07:49 +0700

    Cannot scroll in iphoneSE

--- anlacxa_app/lib/screens/login_screen.dart
+++ anlacxa_app/lib/screens/login_screen.dart
@@ -38,135 +38,139 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Stack(
-          children: [
-            Image.asset('assets/images/1.png'),
-            Positioned(
-              top: 270,
-              left: 0,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/1.png'), fit: BoxFit.fill),
+            ),
+          ),
+          Positioned(
+            top: 270,
+            left: 0,
+            child: SingleChildScrollView(
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
+                height: MediaQuery.of(context).size.height - 270,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: Colors.white,
                 ),
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 30),
-                    child: Column(
-                      children: [
-                        Text(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 30),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.green.shade500,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text(
+                        'Some Text Here',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        onTapOutside: (event) {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                        },
+                        controller: _usernameController,
+                        autofocus: false,
+                        style: const TextStyle(fontSize: 18),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.green.shade100,
+                          hintText: 'Username',
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(14, 8, 14, 8),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.green.shade100,
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.green.shade100),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      const SizedBox(height: 30),
+                      TextField(
+                        onTapOutside: (event) {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                        },
+                        controller: _passwordController,
+                        autofocus: false,
+                        obscureText: isPasswordVisible ? false : true,
+                        style: const TextStyle(fontSize: 18),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.green.shade100,
+                          hintText: 'Password',
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(14, 8, 14, 8),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.green.shade100,
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.green.shade100),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: togglePassword,
+                            icon: passwordIcon,
+                          ),
+                        ),
+                        textInputAction: TextInputAction.done,
+                      ),
+                      const SizedBox(height: 60),
+                      ElevatedButton(
+                        onPressed: () {
+                          print(_usernameController);
+                          print(_passwordController);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green.shade500,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          minimumSize: const Size(double.infinity, 40),
+                        ),
+                        child: const Text(
                           'Login',
                           style: TextStyle(
-                            color: Colors.green.shade500,
-                            fontSize: 40,
+                            fontSize: 20,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const Text(
-                          'Some Text Here',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        TextField(
-                          onTapOutside: (event) {
-                            FocusManager.instance.primaryFocus?.unfocus();
-                          },
-                          controller: _usernameController,
-                          autofocus: false,
-                          style: const TextStyle(fontSize: 18),
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.green.shade100,
-                            hintText: 'Username',
-                            contentPadding:
-                                const EdgeInsets.fromLTRB(14, 8, 14, 8),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.green.shade100,
-                              ),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.green.shade100),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                          ),
-                          textInputAction: TextInputAction.next,
-                        ),
-                        const SizedBox(height: 30),
-                        TextField(
-                          onTapOutside: (event) {
-                            FocusManager.instance.primaryFocus?.unfocus();
-                          },
-                          controller: _passwordController,
-                          autofocus: false,
-                          obscureText: isPasswordVisible ? false : true,
-                          style: const TextStyle(fontSize: 18),
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.green.shade100,
-                            hintText: 'Password',
-                            contentPadding:
-                                const EdgeInsets.fromLTRB(14, 8, 14, 8),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.green.shade100,
-                              ),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.green.shade100),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            suffixIcon: IconButton(
-                              onPressed: togglePassword,
-                              icon: passwordIcon,
-                            ),
-                          ),
-                          textInputAction: TextInputAction.done,
-                        ),
-                        const SizedBox(height: 60),
-                        ElevatedButton(
-                          onPressed: () {
-                            print(_usernameController);
-                            print(_passwordController);
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green.shade500,
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            minimumSize: const Size(double.infinity, 40),
-                          ),
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
